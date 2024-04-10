735bcef9ad1d62deae2a27fbcfa42c580abc3dbf
Alex Sereno <2569805+alexsereno@users.noreply.github.com>
2024-04-10 11:07:37 -0500

    improve rss

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Alexander Sereno - Letters";
-export const SITE_DESCRIPTION = "It's a blog.";
+export const SITE_DESCRIPTION =
+    "An attempt to fashion myself as a man of letters.";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,25 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
     const posts = await getCollection("blog");
     return rss({
+        stylesheet: "/rss/styles.xsl",
+
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
         site: context.site,
         items: posts.map((post) => ({
-            ...post.data,
-            content: post.body,
             link: `/${post.slug}/`,
+            // Note: this will not process components or JSX expressions in MDX files.
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+            }),
+            ...post.data,
         })),
     });
 }
